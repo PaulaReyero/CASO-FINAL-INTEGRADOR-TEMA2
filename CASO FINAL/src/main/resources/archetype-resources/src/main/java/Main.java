2387be93c84ffd5a21427c65c991a243cb4c73e8
PaulaReyero <159237020+PaulaReyero@users.noreply.github.com>
2024-02-28 08:47:13 +0000

    CAMBIOS

---
+++ CASO FINAL/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+
+
+    public class habitat{
+        private int temperatura;
+        private int humedad;
+        private String limpieza;
+    }
+    public class acuatico extends habitat{
+
+    }
+}

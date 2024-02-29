bb15b716d91c1e1cb95e11a79d3f69adedd57c4e
PaulaReyero <159237020+PaulaReyero@users.noreply.github.com>
2024-02-29 12:03:08 +0000

    cambios

---
+++ CASO FINAL/src/main/resources/archetype-resources/src/Gestion_habitats/habitats.java
@@ -0,0 +1,14 @@
+package Gestion_habitats;
+
+public class habitats{
+
+    float temperatura;
+    float humedad;
+    boolean limpieza;
+
+    public habitats(float temperatura, float humedad, booleann limpieza){
+        this.temperatura = temperatura;
+        this.hhumedad = humedad;
+        this.limpieza = limpieza;
+    }
+}

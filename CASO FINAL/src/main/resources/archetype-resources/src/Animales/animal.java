56af0dc0660c0c1ffc99854596f967f756edd3d7
PaulaReyero <159237020+PaulaReyero@users.noreply.github.com>
2024-03-11 07:49:28 +0000

    settygett

--- CASO FINAL/src/main/resources/archetype-resources/src/Animales/animal.java
+++ CASO FINAL/src/main/resources/archetype-resources/src/Animales/animal.java
@@ -38,9 +38,70 @@ public class animal{
         }
         return id;
     }
+    public String getId(){
+        return id;
+    }  //tengo que hacer un setter para id?
     public String getNombre(){
         return nombre;
     }
-
-
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public String getEspecie(){
+        return especie;
+    }
+    public void setEspecie(String especie){
+        this.especie = especie;
+    }
+    public boolean getSexo(){
+        if(sexo= TRUE){
+            return "Macho";
+        }else{
+            return "Hembra";
+        }
+    }
+    public void setSexo(boolean sexo){
+        this.sexo = sexo;
+    }
+    public float getPeso(){
+        return peso;
+    }
+    public void setPeso(float peso){
+        this.peso = peso;
+    }
+    public String getHabitat(){
+        return habitat;
+    }
+    public void setHabitat(String habitat){
+        this.habitat = habitat;
+    }
+    public String getAlimentacion(){
+        return alimentacion;
+    }
+    public void setAlimentacion(String alimentacion)
+    this.alimentacion = alimentacion;
+    public String getReproduccion(){
+        return reproduccion;
+    }
+    public void setReproduccion(String reproduccion){
+        this.reproduccion = reproduccion;
+    }
+    public boolean getSalud(){
+        return salud;
+    }
+    public void setSalud(boolean salud){
+        this.salud = salud;
+    }
+    public boolean getComportamiento(){
+        return comportamiento;
+    }
+    public void setComportamiento(boolean comportamiento){
+        this.comportamiento = comportamiento;
+    }
 }
